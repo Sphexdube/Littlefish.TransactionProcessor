5823bbb2c0e10d2281c83da0fdcc457d09885360
Siphesihle Dube <siphesihledube@192.168.1.13>
2026-02-25 22:59:09 +0200

    Initial commit

---
+++ test/Transaction.Tests.Integration/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace Transaction.Tests.Integration;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
