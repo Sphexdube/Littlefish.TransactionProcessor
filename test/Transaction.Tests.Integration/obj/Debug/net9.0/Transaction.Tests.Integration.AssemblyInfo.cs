ca729f0ed91abd7b6b33966a2408d068d19547df
siphesihledube <se.dube28@gmail.com>
2026-02-26 13:03:49 +0200

    Latest changes (cleanup)

--- test/Transaction.Tests.Integration/obj/Debug/net9.0/Transaction.Tests.Integration.AssemblyInfo.cs
+++ test/Transaction.Tests.Integration/obj/Debug/net9.0/Transaction.Tests.Integration.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Transaction.Tests.Integration")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c6b2c643f98d0b89fa2049d71540f80a5e42541")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ccb574432522dff98301e3076f575bf2ba49335")]
 [assembly: System.Reflection.AssemblyProductAttribute("Transaction.Tests.Integration")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Transaction.Tests.Integration")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
